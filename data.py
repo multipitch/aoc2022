a87543a7d86e9ff5b9f5421f7922f39736b4313c
multipitch <14188124+multipitch@users.noreply.github.com>
2022-12-08 22:26:06 +0000

    Solve Day 08, refactor to reduce i/o boilerplate.

--- data.py
+++ data.py
@@ -5,27 +5,27 @@ AOC 2022 Data.
 """
 
 DAY_02_SCORES_1 = {
-    "A X\n": 4,
-    "A Y\n": 8,
-    "A Z\n": 3,
-    "B X\n": 1,
-    "B Y\n": 5,
-    "B Z\n": 9,
-    "C X\n": 7,
-    "C Y\n": 2,
-    "C Z\n": 6,
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6,
 }
 
 DAY_02_SCORES_2 = {
-    "A X\n": 3,
-    "A Y\n": 4,
-    "A Z\n": 8,
-    "B X\n": 1,
-    "B Y\n": 5,
-    "B Z\n": 9,
-    "C X\n": 2,
-    "C Y\n": 6,
-    "C Z\n": 7,
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
 }
 DAY_03_SCORES = {
     "a": 1,
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 from pathlib import Path
 import heapq
 import re
+import math
 from collections import defaultdict
 from copy import deepcopy
 from typing import Any, Callable
@@ -24,58 +25,61 @@ def register(func: Callable[..., Any]) -> Callable[..., Any]:
     return func
 
 
+def run_problem(func: Callable[[str], tuple[Any, Any]]) -> None:
+    """Run a problem on a downloaded dataset."""
+    day = str(func.__doc__)[4:6]
+    with open(INPUT_FOLDER / f"{day}.txt", encoding="utf-8") as file_obj:
+        raw_string = file_obj.read()
+    results = func(raw_string)
+    for i in (0, 1):
+        print(f"Day {day}, Part {i}: {results[i]}")
+
+
 def run_all() -> None:
     """Run all registered functions."""
     for func in ALL_PROBLEMS:
-        func()
+        run_problem(func)
 
 
 @register
-def day_01() -> None:
+def day_01(data: str) -> tuple[int, int]:
     """Day 01."""
-    with open(INPUT_FOLDER / "01.txt", encoding="utf-8") as file_obj:
-        raw = file_obj.read()
-    inventories = [sum(int(j) for j in i.split()) for i in raw.split("\n\n")]
-    print(f"Day 01, Part 1: {max(inventories)}")
-    print(f"Day 01, Part 2: {sum(heapq.nlargest(n=3, iterable=inventories))}")
+    inventories = [sum(int(j) for j in i.split()) for i in data.split("\n\n")]
+    return max(inventories), sum(heapq.nlargest(n=3, iterable=inventories))
 
 
 @register
-def day_02() -> None:
+def day_02(data: str) -> tuple[int, int]:
     """Day 02."""
-    with open(INPUT_FOLDER / "02.txt", encoding="utf-8") as file_obj:
-        plays = file_obj.readlines()
-    print(f"Day 02, Part 1: {sum(DAY_02_SCORES_1[play]for play in plays)}")
-    print(f"Day 02, Part 2: {sum(DAY_02_SCORES_2[play]for play in plays)}")
+    plays = data.splitlines()
+    return (
+        sum(DAY_02_SCORES_1[play] for play in plays),
+        sum(DAY_02_SCORES_2[play] for play in plays),
+    )
 
 
 @register
-def day_03() -> None:
+def day_03(data: str) -> tuple[int, int]:
     """Day 03."""
-    with open(INPUT_FOLDER / "03.txt", encoding="utf-8") as file_obj:
-        data = [line.strip() for line in file_obj.readlines()]
-
+    lines = data.splitlines()
     common_1 = [
         set(line[: len(line) // 2]).intersection(set(line[len(line) // 2 :]))
-        for line in data
+        for line in lines
     ]
     score_1 = sum(DAY_03_SCORES[letter] for s in common_1 for letter in s)
-    print(f"Day 03, Part 1: {score_1}")
-
     qty = 3
     common_2 = [
-        set.intersection(*[set(data[qty * i + n]) for n in range(qty)])
-        for i in range(len(data) // qty)
+        set.intersection(*[set(lines[qty * i + n]) for n in range(qty)])
+        for i in range(len(lines) // qty)
     ]
     score_2 = sum(DAY_03_SCORES[letter] for s in common_2 for letter in s)
-    print(f"Day 03, Part 2: {score_2}")
+    return score_1, score_2
 
 
 @register
-def day_04() -> None:
+def day_04(data: str) -> tuple[int, int]:
     """Day 04."""
-    with open(INPUT_FOLDER / "04.txt", encoding="utf-8") as file_obj:
-        lines = file_obj.readlines()
+    lines = data.splitlines()
     ranges = [[int(s) for s in re.findall(r"\d+", line)] for line in lines]
     total_overlaps = sum(
         1
@@ -90,19 +94,15 @@ def day_04() -> None:
         or (x1 <= x2 <= y1)
         or (x1 <= y2 <= y1)
     )
-    print(f"Day 04, Part 1: {total_overlaps}")
-    print(f"Day 04, Part 2: {partial_overlaps}")
+    return total_overlaps, partial_overlaps
 
 
 @register
-def day_05() -> None:
+def day_05(data: str) -> tuple[str, str]:
     """Day 05."""
-    with open(INPUT_FOLDER / "05.txt", encoding="utf-8") as file_obj:
-        raw = file_obj.read()
-    [raw_stacks, raw_moves] = raw.split("\n\n")
-
+    [raw_stacks, raw_moves] = data.split("\n\n")
     stacks = defaultdict(list)
-    levels = raw_stacks.split("\n")[-2::-1]
+    levels = raw_stacks.splitlines()[-2::-1]
     for level in levels:
         for i, crate in enumerate(level[1::4]):
             if crate != " ":
@@ -129,17 +129,15 @@ def day_05() -> None:
             stacks_[destination].extend(takeoff)
         return "".join(stacks_[i][-1] for i in range(1, 10))
 
-    print(f"Day 05, Part 1: {crate_mover_9000()}")
-    print(f"Day 05, Part 2: {crate_mover_9001()}")
+    return crate_mover_9000(), crate_mover_9001()
 
 
 @register
-def day_06() -> None:
+def day_06(data: str) -> tuple[int, int]:
     """Day 06."""
-    with open(INPUT_FOLDER / "06.txt", encoding="utf-8") as file_obj:
-        code = file_obj.readline()
+    code = data.strip()
 
-    def day_06_helper(repeat_len: int) -> int:
+    def day_06_inner(repeat_len: int) -> int:
         for end in range(repeat_len, len(code) + 1):
             if len(set(code[end - repeat_len : end])) == repeat_len:
                 break
@@ -147,44 +145,31 @@ def day_06() -> None:
             raise ValueError
         return end
 
-    print(f"Day 06, Part 1: {day_06_helper(4)}")
-    print(f"Day 06, Part 2: {day_06_helper(14)}")
+    return day_06_inner(4), day_06_inner(14)
 
 
 @register
-def day_07() -> None:
+def day_07(data: str) -> tuple[int, int]:
     """Day 07."""
-    with open(INPUT_FOLDER / "07.txt", encoding="utf-8") as file_obj:
-        lines = file_obj.read().splitlines()
-
     threshold = 100000
     total_space = 70000000
     required_space = 30000000
 
     class Directory:
-        """
-        A directory.
-        Could probably use a bunch of dicts instead of classes, but
-        this makes more sense to read.
-        """
+        """A directory."""
 
         def __init__(self, name: str, parent: Directory | None = None):
             self.name = name
             self.parent = parent
             self.subdirectories: dict[str, Directory] = {}
             self.files: dict[str, int] = {}
-            self.size = 0
+            self.size: int = 0
 
     root = Directory(name="/")
     all_directories: set[Directory] = set()
     location = root
 
-    # This approach assumes that the user creating the data traversed
-    # directories in a depth-first search, allowing us to compute the
-    # total directory sizes on "$ cd .." operations.
-    # Otherwise, would have to keep lists of seen files or re-traverse a
-    # second time to totalize sizes.
-    for line in lines:
+    for line in data.splitlines():
         match line.split():
             case ["$", "cd", "/"]:
                 location = root
@@ -208,19 +193,60 @@ def day_07() -> None:
             case _:
                 raise ValueError
 
-    # The data doesn't cd back to root at the end; we need to do this to
-    # correctly compute totals for all directories back up to root.
     while location != root:
         if location.parent:
             location.parent.size += location.size
             location = location.parent
 
-    total = sum([dir.size for dir in all_directories if dir.size <= threshold])
-    print(f"Day 07, Part 1: {total}")
-
+    part1 = sum([dir.size for dir in all_directories if dir.size <= threshold])
     min_del = max(0, required_space - (total_space - root.size))
-    del_size = min(dir.size for dir in all_directories if dir.size >= min_del)
-    print(f"Day 07, Part 2: {del_size}")
+    part2 = min(dir.size for dir in all_directories if dir.size >= min_del)
+    return part1, part2
+
+
+@register
+def day_08(data: str) -> tuple[int, int]:
+    """Day 08."""
+    heights = {}
+    i = 0
+    j = 0
+    for i, row in enumerate(data.splitlines()):
+        for j, height in enumerate(row):
+            heights[i, j] = int(height)
+
+    def is_visible(x: int, y: int) -> bool:
+        """Check if location is visible."""
+        visible = (
+            heights[x, y] > max([heights[a, y] for a in range(x)], default=-1)
+            or heights[x, y]
+            > max([heights[a, y] for a in range(x + 1, i + 1)], default=-1)
+            or heights[x, y]
+            > max([heights[x, b] for b in range(y)], default=-1)
+            or heights[x, y]
+            > max([heights[x, b] for b in range(y + 1, j + 1)], default=-1)
+        )
+        return visible
+
+    def score(x: int, y: int) -> int:
+        """Get score for a location."""
+        loc_scores = []
+        for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+            dir_score = 0
+            while True:
+                next_height = heights.get(
+                    (x + (1 + dir_score) * dx, y + (1 + dir_score) * dy)
+                )
+                if next_height is None:
+                    break
+                dir_score += 1
+                if next_height >= heights[x, y]:
+                    break
+            loc_scores.append(dir_score)
+        return math.prod(loc_scores)
+
+    qty_visible = sum([is_visible(*location) for location in heights])
+    max_score = max(score(*location) for location in heights)
+    return qty_visible, max_score
 
 
 if __name__ == "__main__":
